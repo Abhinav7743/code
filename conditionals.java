8843f682f6627910638fa5df992b3b5292db13f0
Abhinav7743 <164772183+Abhinav7743@users.noreply.github.com>
2024-04-05 12:18:49 +0530

    conditional.java

---
+++ conditionals.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class conditionals {
+    public static void main(String[] args) {
+       int age = 15;
+
+      // if(age!=18) {     // not equal !=.
+        if (age==18) {
+        System.out.println("Yes boy you can drive!");
+
+       }
+       else {
+        System.out.println("No boy you cannot drive yet !");
+       }
+
+    }
+}
