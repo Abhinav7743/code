85efeb074dcfa17fd7d38c683e4e0bc9e512ca07
Abhinav7743 <164772183+Abhinav7743@users.noreply.github.com>
2024-04-05 12:34:18 +0530

    odd.java

---
+++ odd.java
@@ -0,0 +1,15 @@
+public class odd {
+    public static void main(String[] args){
+
+        int x=11;
+        if (x%2==0)
+{
+    System.out.println(x +" is even");
+}
+else
+{
+    System.out.println(x +" is odd");
+}
+System.out.println("Hardwork is better then smart work");
+    }
+}
