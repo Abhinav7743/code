a8ccbfea09d5bc10293d7c5c59b28e8500c056c5
Abhinav7743 <164772183+Abhinav7743@users.noreply.github.com>
2024-04-05 12:19:12 +0530

    in.java

---
+++ in.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public interface in {
+
+    static int nextInt() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextInt'");
+    }
+
+}
