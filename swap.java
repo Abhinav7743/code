56c93d70376c553310f5114189f82af11aea47cc
Abhinav7743 <164772183+Abhinav7743@users.noreply.github.com>
2024-04-05 12:29:23 +0530

    swap.java

---
+++ swap.java
@@ -0,0 +1,29 @@
+public class swap {
+  public static void main(String[] args) {
+    int a = 849;
+    int b = 949;
+
+    //Swap the code
+   // int temp = a;
+    // a=b;
+   // b = temp;
+
+   swap(a, b);
+
+    System.out.println(a + " " + b);
+
+    String naam = "Abhinav Singh";
+      changeName(naam);
+    System.out.println(naam);
+  }
+  static void changeName(String naam) {
+    naam = "Puneet Rana";
+  }
+  static void swap(int num1, int num2) {
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    // this change will only be valid in this function scope only.
+  }
+}
