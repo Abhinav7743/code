b279b78696dfcddd58f9f3c1712c957321a2486e
Abhinav7743 <164772183+Abhinav7743@users.noreply.github.com>
2024-04-05 12:17:02 +0530

    logical.java

---
+++ logical.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+public class logical {
+    public static void main(String[] args) {
+    System.out.println("For Logical And");   
+    boolean b = false;
+   
+   // if (a && b && c) 
+     //   System.out.println("Y");
+
+   // }  
+   // else {
+    //    System.out.println("N");
+  //   }
+  //  System.out.println("For Logical OR...");   
+   
+  //  if (a || b  ) {
+  //      System.out.println("Y");
+
+   // }   
+    //else {
+    //    System.out.println("N");
+    //}
+    System.out.println("Logical Not");
+    System.out.println("Not(a) is ");
+    System.out.println("!a");
+    System.out.println("Not(b) is ");
+    System.out.println(!b);
+    }
+
+}
