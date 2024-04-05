7732c7d6bbcf4135d533dab6c0576ee5ea4f1994
Abhinav7743 <164772183+Abhinav7743@users.noreply.github.com>
2024-04-05 12:30:22 +0530

    gratter.java

---
+++ grater.java
@@ -0,0 +1,19 @@
+public class grater {
+   public static void main(String[] args) {
+
+    int n1=10;
+    int n2=12;
+     if(n1==n2)
+     {
+        System.out.println(n2 + "equal then"+ n2);
+     }
+     else if(n1>n2)
+     {
+        System.out.println(n1 + "greater then"+ n2);
+     }
+     else
+     {
+        System.out.println(n1 + "smaller then"+ n2);
+     }
+   }
+}
